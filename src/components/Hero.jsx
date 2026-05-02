de4a1e3d4af3eb7f74d6d99bcd96c8db977b0543
Mararovsek99 <andrej.marovsek99@gmail.com>
2026-05-02 22:17:35 +0200

    installed react icons and added subtitle to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import GlobeIcon from "./GlobeIcon";
+import { GoArrowDownRight } from "react-icons/go";
 
 const Hero = () => {
   return (
@@ -20,6 +21,15 @@ const Hero = () => {
           </div>
           <div className=" bg-[#999d9e] rounded-full h-20 w-20 m-5">
             <GlobeIcon className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+          </div>
+        </div>
+        <div className="absolute right-0 top-1/2   -translate-y-40  flex flex-col gap-10">
+          <div>
+            <GoArrowDownRight className="h-10 w-10 mb-2" />
+          </div>
+          <div className="flex flex-col m-2 mr-20 gap-2">
+            <span className="text-5xl">Web Developer</span>
+            <span className="text-4xl">with Engineering Expertise</span>
           </div>
         </div>
       </section>
