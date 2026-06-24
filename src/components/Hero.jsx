5f68f8b124f4427ae21f3343a8a1513f32fa0208
Mararovsek99 <andrej.marovsek99@gmail.com>
2026-06-24 22:09:28 +0200

    deleted double code for subtitle web engineer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
         <img
           src="/hero_andrej.webp"
           alt="author of the page"
-          className="h-screen absolute left-1/2 top-0"
+          className="h-screen absolute left-[60%] md:left-[50%] top-[10%]"
           style={{
             transform: `translateX(-50%) translateY(${scrollY * 0.2}px)`,
             objectFit: "cover",
@@ -46,23 +46,17 @@ const Hero = () => {
         <div className="absolute right-4 bottom-4 md:hidden rounded-full h-20 w-20 flex items-center justify-center shadow-xl shadow-black/30">
           <GlobeIcon className="h-10 w-10" />
         </div>
-        <div className="md:absolute md:right-0 md:top-1/2 md:-translate-y-25 md:lg:-translate-y-40 md:-translate-y-20 md:-translate-x-10 md:flex md:flex-col md:gap-5 md:lg:gap-10 ">
+        <div className="absolute left-0 bottom-0 flex flex-col gap-3 ml-4 mb-4 md:left-auto md:right-0 md:top-1/2 md:bottom-auto md:-translate-y-1/2 md:ml-0 md:mr-10 md:gap-5 lg:gap-10">
           <div>
-            <GoArrowDownRight className="h-10 w-10 mb-2" />
+            <GoArrowDownRight className="h-8 w-8 mb-2 md:h-10 md:w-10" />
           </div>
-          <div className="flex flex-col m-2 md:mr-10 lg:mr-20 gap-2">
-            <span className="md:text-4xl lg:text-5xl">Web Developer</span>
-            <span className="lg:text-4xl">with Engineering Expertise</span>
-          </div>
-        </div>
-
-        <div className="absolute left-0 bottom-0 md:hidden flex flex-col gap-3 ml-4 mb-4">
-          <div>
-            <GoArrowDownRight className="h-8 w-8 mb-2" />
-          </div>
-          <div className="flex flex-col gap-1">
-            <span className="text-3xl">Web Developer</span>
-            <span className="text-lg">with Engineering Expertise</span>
+          <div className="flex flex-col gap-1 md:gap-2">
+            <span className="text-3xl md:text-4xl lg:text-5xl">
+              Web Developer
+            </span>
+            <span className="text-lg md:text-xl lg:text-4xl">
+              with Engineering Expertise
+            </span>
           </div>
         </div>
       </section>
